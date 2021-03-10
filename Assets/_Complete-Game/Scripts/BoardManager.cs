6fd198d5b26dbb6f4ff712ea6e7d50f2d798f9d9
Mihail Kanchev <colty.gaming@gmail.com>
2021-03-10 11:57:45 +0100

    init

---
+++ Assets/_Complete-Game/Scripts/BoardManager.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+namespace Completed
+	
+{
+	
+	public class BoardManager : MonoBehaviour
+	{
+		// Using Serializable allows us to embed a class with sub properties in the inspector.
+		[Serializable]
+		public class Count
+		{
+			public int minimum; 			//Minimum value for our Count class.
+			public int maximum; 			//Maximum value for our Count class.
+			
+			
+			//Assignment constructor.
+			public Count (int min, int max)
+			{
+				minimum = min;
+				maximum = max;
+			}
+		}
+		
+		public GameObject exit;											//Entrance to a dungeon
+		public int columns = 5; 										//Number of columns in our game board.
+		public int rows = 5;											//Number of rows in our game board.
+		public GameObject[] outerWallTiles;								//Boundry walls of the dungeon
+		public GameObject[] floorTiles;									//Array of floor prefabs.
+		public GameObject[] wallTiles;									//Array of floor prefabs.
+		private Transform boardHolder;									//A variable to store a reference to the transform of our Board object.
+		private Dictionary<Vector2, Vector2> gridPositions = new Dictionary<Vector2, Vector2>();	//A list of possible locations to place tiles.
+		private Transform dungeonBoardHolder;
+		private Dictionary<Vector2, Vector2> dungeonGridPositions;		//Dictionary to hold the results from DungeonManager class
+		
+		//Sets up the outer walls and floor (background) of the game board.
+		public void BoardSetup ()
+		{
+			//Instantiate Board and set boardHolder to its transform.
+			boardHolder = new GameObject ("Board").transform;
+			
+			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+			for(int x = 0; x < columns; x++)
+			{
+				//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+				for(int y = 0; y < rows; y++)
+				{
+
+					gridPositions.Add(new Vector2(x,y), new Vector2(x,y));
+
+					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+					GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
+					
+					//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+					GameObject instance =
+						Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+					
+					//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+					instance.transform.SetParent (boardHolder);
+				}
+			}
+		}
+
+		public void addToBoard(int horizontal, int vertical){
+			if(horizontal == 1){
+				int x = (int)Player.position.x;
+				int sightX = x + 2;
+				for(x += 1; x <= sightX; x++){
+					int y = (int)Player.position.y;
+					int sightY = y + 1;
+					for(y -= 1; y  <= sightY; y++){
+						addTiles(new Vector2 (x,y));
+					}
+				}
+			}
+			else if(horizontal == -1){
+				int x = (int)Player.position.x;
+				int sightX = x - 2;
+				for(x -= 1; x >= sightX; x--){
+					int y = (int)Player.position.y;
+					int sightY = y + 1;
+					for(y -= 1; y  <= sightY; y++){
+						addTiles(new Vector2 (x,y));
+					}
+				}
+			}
+			else if(vertical == 1){
+				int y = (int)Player.position.y;
+				int sightY = y + 2;
+				for(y += 1; y <= sightY; y++){
+					int x = (int)Player.position.x;
+					int sightX = x + 1;
+					for(x -= 1; x  <= sightX; x++){
+						addTiles(new Vector2 (x,y));
+					}
+				}
+			}
+			else if(vertical == -1){
+				int y = (int)Player.position.y;
+				int sightY = y - 2;
+				for(y -= 1; y >= sightY; y--){
+					int x = (int)Player.position.x;
+					int sightX = x + 1;
+					for(x -= 1; x  <= sightX; x++){
+						addTiles(new Vector2 (x,y));
+					}
+				}
+			}
+		}
+		private void addTiles(Vector2 tileToAdd){
+			if(!gridPositions.ContainsKey(tileToAdd)) {
+				gridPositions.Add(tileToAdd, tileToAdd);
+				GameObject toInstantiate = floorTiles[Random.Range(0,floorTiles.Length)];
+				GameObject instance = Instantiate(toInstantiate, new Vector3(tileToAdd.x, tileToAdd.y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent(boardHolder);
+				if(Random.Range(0,3) == 1){
+					toInstantiate = wallTiles[Random.Range(0,wallTiles.Length)];
+					instance = Instantiate(toInstantiate, new Vector3(tileToAdd.x,tileToAdd.y, 0f),Quaternion.identity) as GameObject;
+					instance.transform.SetParent(boardHolder);
+				}
+
+				if (Random.Range (0, 10) == 1) {
+					toInstantiate = exit;
+					instance = Instantiate (toInstantiate, new Vector3 (tileToAdd.x, tileToAdd.y, 0f), Quaternion.identity) as GameObject;
+					instance.transform.SetParent (boardHolder);
+				}
+			}
+		}
+
+		 public void SetDungeonBoard (Dictionary<Vector2,TileType>  dungeonTiles, int bound, Vector2 endPos) {
+			boardHolder.gameObject.SetActive (false);
+			dungeonBoardHolder = new GameObject ("Dungeon").transform;
+			GameObject toInstantiate, instance;
+ 
+ 			foreach(KeyValuePair<Vector2,TileType> tile in dungeonTiles) {
+				toInstantiate = floorTiles [Random.Range (0, floorTiles.Length)];
+				instance = Instantiate (toInstantiate, new Vector3 
+				(tile.Key.x, tile.Key.y, 0f), Quaternion.identity) as GameObject;
+				instance.transform.SetParent (dungeonBoardHolder);
+			}
+
+			for (int x = -1; x < bound + 1; x++) {
+				for (int y = -1; y < bound + 1; y++) {
+ 					if (!dungeonTiles.ContainsKey(new Vector2(x, y))) {
+						toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
+						instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+						instance.transform.SetParent (dungeonBoardHolder);
+					}
+ 				}
+ 			}			
+
+			toInstantiate = exit;
+			instance = Instantiate (toInstantiate, new Vector3 (endPos.x, endPos.y, 0f), Quaternion.identity) as GameObject;
+			instance.transform.SetParent (dungeonBoardHolder);
+ 		}
+
+		public void SetWorldBoard () {
+			Destroy (dungeonBoardHolder.gameObject);
+			boardHolder.gameObject.SetActive (true);
+		}
+	}
+}
---
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	//Allows us to use UI.
+using UnityEngine.SceneManagement;
+
+namespace Completed
+{
+	//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+	public class Player : MovingObject
+	{
+		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
+		public int pointsPerFood = 10;				//Number of points to add to player food points when picking up a food object.
+		public int pointsPerSoda = 20;				//Number of points to add to player food points when picking up a soda object.
+		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
+		public bool onWorldBoard;
+		public bool dungeonTransition;
+		public static Vector2 position;
+		public Text foodText;						//UI Text to display current player food total.
+		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
+		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
+		public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
+		public AudioClip eatSound2;					//2 of 2 Audio clips to play when player collects a food object.
+		public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
+		public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
+		public AudioClip gameOverSound;				//Audio clip to play when player dies.
+		
+		private Animator animator;					//Used to store a reference to the Player's animator component.
+		private int food;                           //Used to store player food points total during level.
+#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+        private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
+#endif
+		
+		
+		//Start overrides the Start function of MovingObject
+		protected override void Start ()
+		{
+			//Get a component reference to the Player's animator component
+			animator = GetComponent<Animator>();
+			
+			//Get the current food point total stored in GameManager.instance between levels.
+			food = GameManager.instance.playerFoodPoints;
+			
+			//Set the foodText to reflect the current player food total.
+			foodText.text = "Food: " + food;
+			
+			position.x = position.y = 2;
+
+			onWorldBoard = true;
+			dungeonTransition = false;
+
+			//Call the Start function of the MovingObject base class.
+			base.Start ();
+		}
+		
+		
+		//This function is called when the behaviour becomes disabled or inactive.
+		private void OnDisable ()
+		{
+			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+			GameManager.instance.playerFoodPoints = food;
+		}
+		
+		
+		private void Update ()
+		{
+			//If it's not the player's turn, exit the function.
+			if(!GameManager.instance.playersTurn) return;
+			
+			int horizontal = 0;  	//Used to store the horizontal move direction.
+			int vertical = 0;		//Used to store the vertical move direction.
+			
+			bool canMove = false;
+
+			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+			vertical = (int) (Input.GetAxisRaw ("Vertical"));
+
+			if (horizontal != 0)
+				vertical = 0;
+
+			if(horizontal != 0 || vertical != 0){
+				if(!dungeonTransition){
+					canMove = AttemptMove<Wall>(horizontal,vertical);
+					if(canMove){
+						position.x += horizontal;
+						position.y += vertical;
+						GameManager.instance.updateBoard(horizontal,vertical);
+					}
+				}
+			}
+		}
+
+		private void GoDungeonPortal(){
+			if(onWorldBoard){
+				onWorldBoard = false;
+				GameManager.instance.enterDungeon();
+				transform.position = DungeonManager.startPos;
+			}
+			else{
+				onWorldBoard = true;
+				GameManager.instance.exitDungeon();
+				transform.position = position;
+			}
+		}
+		
+		//AttemptMove overrides the AttemptMove function in the base class MovingObject
+		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+		protected override bool AttemptMove <T> (int xDir, int yDir)
+		{
+			bool hit = base.AttemptMove <T> (xDir, yDir);
+			//Set the playersTurn boolean of GameManager to false now that players turn is over.
+			GameManager.instance.playersTurn = false;
+			return hit;
+		}
+		
+		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
+		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+		protected override void OnCantMove <T> (T component)
+		{
+			//Set hitWall to equal the component passed in as a parameter.
+			Wall hitWall = component as Wall;
+			
+			//Call the DamageWall function of the Wall we are hitting.
+			hitWall.DamageWall (wallDamage);
+			
+			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
+			animator.SetTrigger ("playerChop");
+		}
+		
+		//LoseFood is called when an enemy attacks the player.
+		//It takes a parameter loss which specifies how many points to lose.
+		public void LoseFood (int loss)
+		{
+			//Set the trigger for the player animator to transition to the playerHit animation.
+			animator.SetTrigger ("playerHit");
+			
+			//Subtract lost food points from the players total.
+			food -= loss;
+			
+			//Update the food display with the new total.
+			foodText.text = "-"+ loss + " Food: " + food;
+			
+			//Check to see if game has ended.
+			CheckIfGameOver ();
+		}
+		
+		
+		//CheckIfGameOver checks if the player is out of food points and if so, ends the game.
+		private void CheckIfGameOver ()
+		{
+			//Check if food point total is less than or equal to zero.
+			if (food <= 0) 
+			{
+				//Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
+				SoundManager.instance.PlaySingle (gameOverSound);
+				
+				//Stop the background music.
+				SoundManager.instance.musicSource.Stop();
+				
+				//Call the GameOver function of GameManager.
+				GameManager.instance.GameOver ();
+			}
+		}
+
+		private void OnTriggerEnter2D(Collider2D other){
+			if(other.tag == "Exit"){
+				dungeonTransition = true;
+				Invoke("GoDungeonPortal",.5f);
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
+
